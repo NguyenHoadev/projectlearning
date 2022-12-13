c7728ffd74e6a8c6a39ca9a15d982b608093efdc
NguyenHoadev <guitarnguyen1989@gmail.com>
2022-12-13 21:16:03 +0700

    finish start

---
+++ .history/customize-cra-config_20221213191526.js
@@ -0,0 +1,33 @@
+import { theme } from './src/config/theme/themeVariables';
+
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          path: false,
+        },
+      },
+    },
+    test: /\.m?jsx?$/,
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              ...theme,
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
+
+
---
+++ .history/customize-cra-config_20221213191608.js
@@ -0,0 +1,70 @@
+import { theme } from './src/config/theme/themeVariables';
+
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+
+      webpackConfig.plugins.forEach(plugin => {
+        if (plugin instanceof InlineChunkHtmlPlugin) {
+          plugin.tests =  [ /.+[.]js/ ]
+        }
+      })
+
+      const oneOfRuleIdx = webpackConfig.module.rules.findIndex(rule => !!rule.oneOf);
+      webpackConfig.module.rules[oneOfRuleIdx].oneOf.forEach(loader => {
+        if (loader.test && loader.test.test && (loader.test.test("test.module.css") || loader.test.test("test.module.scss"))) {
+          loader.use.forEach(use => {
+            if (use.loader && use.loader.includes('mini-css-extract-plugin')) {
+              use.loader = require.resolve('style-loader');
+            }
+          })
+        }
+      })
+      return webpackConfig
+    }
+  },,
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              ...theme,
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
+
+const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+
+      webpackConfig.plugins.forEach(plugin => {
+        if (plugin instanceof InlineChunkHtmlPlugin) {
+          plugin.tests =  [ /.+[.]js/ ]
+        }
+      })
+
+      const oneOfRuleIdx = webpackConfig.module.rules.findIndex(rule => !!rule.oneOf);
+      webpackConfig.module.rules[oneOfRuleIdx].oneOf.forEach(loader => {
+        if (loader.test && loader.test.test && (loader.test.test("test.module.css") || loader.test.test("test.module.scss"))) {
+          loader.use.forEach(use => {
+            if (use.loader && use.loader.includes('mini-css-extract-plugin')) {
+              use.loader = require.resolve('style-loader');
+            }
+          })
+        }
+      })
+      return webpackConfig
+    }
+  },
+}
